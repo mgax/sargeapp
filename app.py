0fb09b87c8891e2909c29fd71af30311ea6564a2
Alex Morega <alex@grep.ro>
2012-11-14 19:48:23 +0200

    enable werkzeug logging

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ def create_app():
 
 if __name__ == '__main__':
     logging.basicConfig()
+    logging.getLogger('werkzeug').setLevel(logging.INFO)
     port = int(sys.argv[1]) if len(sys.argv) > 1 else 5000
     app = create_app()
     app.run(port=port)
